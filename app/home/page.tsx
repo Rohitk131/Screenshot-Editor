b488116c9d3c0b98eea8a22d6765860f0bde0add
Gutsy <113338596+Rohitk131@users.noreply.github.com>
2024-09-25 14:30:09 +0530

    updated homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,109 +3,104 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { ChevronRight } from 'lucide-react';
 import Pricing from '../components/pricing';
-
+import FAQ from '../components/faq';
+import IconCloud from '../components/logoCloud'; 
 const logos = [
-    {
-      name: 'Vercel',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881430/vercel_wordmark_dark_mhv8u8.svg',
-    },
-    {
-      name: 'Nextjs',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715881475/nextjs_logo_dark_gfkf8m.svg',
-    },
-    {
-      name: 'Prime',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/t2awrrfzdvmg1chnzyfr.svg',
-    },
-    {
-      name: 'Trustpilot',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tkfspxqmjflfllbuqxsi.svg',
-    },
-    {
-      name: 'Webflow',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/nymiivu48d5lywhf9rpf.svg',
-    },
-  
-    {
-      name: 'Airbnb',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/pmblusboe7vkw8vxdknx.svg',
-    },
-    {
-      name: 'Tina',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276560/logos/afqhiygywyphuou6xtxc.svg',
-    },
-    {
-      name: 'Stackoverflow',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/ts1j4mkooxqmscgptafa.svg',
-    },
-    {
-      name: 'mistral',
-      url: 'https://res.cloudinary.com/dfhp33ufc/image/upload/v1715276558/logos/tyos2ayezryjskox3wzs.svg',
-    },
-  ]
-  
+  // Your logos array remains unchanged
+];
+
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-purple-50">
+    <div className="bg-gradient-to-br from-indigo-100 via-white to-purple-100">
       <Navbar />
       <div className="container mx-auto px-4">
-        <main className="max-w-5xl mx-auto py-20 text-center">
-          <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold mb-6 text-gray-900 leading-tight">
-            Supercharge Your
-            <br />
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-600">
-              Screenshots
-            </span> in{" "}
-            <span className="relative inline-block">
-              <span className="relative z-10 font-extrabold text-yellow-500 crayon-highlight px-2">
-                Seconds
+        <main className="w-full mx-auto pt-24 text-center">
+          <div className="h-1/2">
+            <h1 className="text-6xl md:text-6xl lg:text-7xl font-extrabold mb-8 text-gray-900 leading-tight">
+              Supercharge Your
+              <br />
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-purple-600">
+                Screenshots
+              </span> in{" "}
+              <span className="relative inline-block">
+                <span className="relative z-10 font-extrabold text-yellow-500 crayon-highlight px-2">
+                  Seconds
+                </span>
               </span>
-            </span>
-          </h1>
-          <p className="text-xl md:text-2xl text-gray-700 mt-6 mb-12 max-w-3xl mx-auto">
-            Boost engagement by up to{" "}
-            <span className="font-semibold text-indigo-600">60%</span> with
-            eye-catching screenshots that captivate your audience.
-          </p>
-          <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-16">
-            <button className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:scale-105 shadow-lg hover:shadow-xl flex items-center">
-              Get Started <ChevronRight className="ml-2 h-5 w-5" />
-            </button>
-            <button className="bg-white text-indigo-600 font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out hover:bg-indigo-50 border-2 border-indigo-600">
-              Watch Demo
-            </button>
-          </div>
-          <div className="w-full py-12 bg-gray-50 rounded-xl shadow-inner">
-            <h3 className="text-xl font-semibold text-gray-700 mb-6">Trusted by industry leaders</h3>
-            <div className="mx-auto w-full px-4 md:px-8">
-              <div
-                className="group relative mt-6 flex gap-6 overflow-hidden p-2"
-                style={{
-                  maskImage:
-                    "linear-gradient(to right, transparent 0%, black 10%, black 90%, transparent 100%)",
-                }}
-              >
-                {Array(5)
-                  .fill(null)
-                  .map((_, index) => (
-                    <div
-                      key={index}
-                      className="flex shrink-0 animate-logo-cloud flex-row justify-around gap-6"
-                    >
-                      {logos.map((logo, key) => (
-                        <img
-                          key={key}
-                          src={logo.url}
-                          className="h-8 w-24 px-2 opacity-70 hover:opacity-100 transition-opacity duration-300"
-                          alt={`${logo.name}`}
-                        />
-                      ))}
-                    </div>
-                  ))}
-              </div>
+            </h1>
+            <p className="text-xl md:text-4xl text-gray-700 mt-6 mb-12 max-w-4xl mx-auto">
+              Boost engagement by up to{" "}
+              <span className="font-semibold text-indigo-600">60%</span> with
+              eye-catching screenshots that captivate your audience.
+            </p>
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-16">
+              <button className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:scale-105 shadow-lg hover:shadow-xl flex items-center">
+                Get Started <ChevronRight className="ml-2 h-5 w-5" />
+              </button>
+              <button className="bg-white text-indigo-600 font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out hover:bg-indigo-50 border-2 border-indigo-600">
+                Watch Demo
+              </button>
             </div>
           </div>
-          <Pricing/>
+          <IconCloud/>
+          <Pricing />
+          <FAQ />
+
+          <footer className="bg-white mt-4 rounded-t-2xl">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-4 lg:py-8 lg:pt-16">
+              <div className="md:flex md:justify-between">
+                <div className="mb-6 md:mb-0">
+                  <a href="https://flowbite.com/" className="flex items-center">
+                    <img src="https://gixland.com/wp-content/uploads/2021/11/logoipsum-logo-17-01.png" className="h-8 me-3" alt="FlowBite Logo" />
+                  </a>
+                </div>
+                <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                  <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                    <ul className="text-gray-500 font-medium">
+                      <li className="mb-4">
+                        <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                      </li>
+                      <li>
+                        <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                    <ul className="text-gray-500 font-medium">
+                      <li className="mb-4">
+                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                      </li>
+                      <li>
+                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                    <ul className="text-gray-500 font-medium">
+                      <li className="mb-4">
+                        <a href="#" className="hover:underline">Privacy Policy</a>
+                      </li>
+                      <li>
+                        <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <hr className="my-6 border-gray-200 lg:my-8" />
+              <div className="sm:flex sm:items-center sm:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+                <div className="flex mt-4 sm:justify-center sm:mt-0">
+                  {/* Social Media Links */}
+                  {/* Include your social media icons here */}
+                </div>
+              </div>
+            </div>
+          </footer>
+
         </main>
       </div>
       <style jsx>{`
